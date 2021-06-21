76e005ba08439872d4e0c05ac29b78fb2a54c8f5
Nayem <nayemk900@gmail.com>
2021-06-21 21:05:57 +0600

    first commit

---
+++ src/components/Home/Header/HeaderBody/HeaderBody.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './HeaderBody.css'
+const HeaderBody = () => {
+    return (
+        <div>
+            <h1 className='text-white'>India's Coolest Travel Community</h1>
+            <h1 className='text-white'> Creating Memories|</h1>
+            <div className='search-container'>
+                <input type="input" name="" id="" placeholder='Where do you wanna go?' className="input-field" />
+                <img src="https://www.wanderon.in/svg/search.svg" alt="" className='search-icon' />
+            </div>
+            <h1 className='text-white'>India's Coolest Travel Community</h1>
+            <h1 className='text-white'>India's Coolest Travel Community</h1>
+
+        </div>
+    );
+};
+
+export default HeaderBody;
---
+++ src/components/Home/Header/Navbar/Navbar.js
@@ -0,0 +1,47 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { Button, Container, Image, Nav, Navbar } from 'react-bootstrap';
+import './Navbar.css'
+const NavBar = () => {
+  const [isSticky, setSticky] = useState(false);
+  const [isCollapsed , setCollapsed] = useState(null);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if(window.scrollY > 50){
+        setSticky(true)
+      }else{
+        setSticky(false)
+      }
+    })
+  }, []);
+
+    return (
+          <Navbar expand="lg" className={ (isSticky || isCollapsed) ? "slide in py-1 show shadow-sm navbar navbar-expand-sm nav-bg navbar-light   fixed-top" : "slide out show navbar navbar-expand-sm navbar-light py-1 fixed-top "}>
+            <Container >
+                <Navbar.Brand  to='/' > <Image style={{height:'33px', width:'43px'}} src='https://www.wanderon.in/wanderon-logo.svg' /> <strong>..</strong></Navbar.Brand>
+                <Navbar.Toggle onClick={  () => setCollapsed(!isCollapsed ? 'show' : null )} aria-controls="basic-navbar-nav"  style={{background:'white'}} />
+                  <Navbar.Collapse id="navbar-nav" >
+                    <Nav className="ml-auto nav-text text-center">
+                      <Nav.Link to='/home' className="mr-3"><strong>Home</strong>
+                      <div className="nav-line"></div>
+                      </Nav.Link>
+                      <Nav.Link href="#about" className="mr-3"><strong>About</strong>
+                      <div className="nav-line"></div>
+                      </Nav.Link>
+                      <Nav.Link href="#service" className="mr-3"><strong>Service</strong>
+                      <div className="nav-line"></div>
+                      </Nav.Link>
+                      <Nav.Link href="#contact" className="mr-3"><strong>Contact</strong>
+                      <div className="nav-line"></div>
+                      </Nav.Link>
+                      <Nav.Link  to='/dashboard/profile' className="mr-3"><strong>Dashboard</strong>
+                      <div className="nav-line"></div>
+                      </Nav.Link>
+                    </Nav>
+                  </Navbar.Collapse>
+             </Container>
+          </Navbar>
+    );
+};
+
+export default NavBar;
