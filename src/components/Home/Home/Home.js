c4c132255c00ad742242438810ef0ca27abfa535
Nayem <nayemk900@gmail.com>
2021-06-21 21:41:45 +0600

    navbar done

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Explore from '../Explore/Explore';
 import Header from '../Header/Header';
 
 const Home = () => {
     return (
         <div>
             <Header/>
+            <Explore/>
         </div>
     );
 };
