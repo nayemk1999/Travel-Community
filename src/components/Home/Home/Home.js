2c3f08a053150d70f1a2eabfac41bfc32c36f4f4
Nayem <nayemk900@gmail.com>
2021-06-23 12:37:40 +0600

    trending part added

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Explores from '../Explore/Explores';
 import Header from '../Header/Header';
+import Trending from '../Trending/Trending';
 
 const Home = () => {
     return (
         <div>
             <Header/>
             <Explores/>
+            <Trending/>
         </div>
     );
 };
